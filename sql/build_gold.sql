4bf39fbeddf7241e5e531f3e83a53733a931d45f
Ethan Huffman <ehuffman10@gmail.com>
2026-02-24 16:18:03 -0500

    add build script for gold layer

---
+++ sql/build_gold.sql
@@ -0,0 +1,7 @@
+\echo 'Building gold.daily_kpis_2024_01...'
+\i sql/gold_daily_kpis_2024_01.sql
+
+\echo 'Building gold.revenue_by_pickup_zone_2024_01...'
+\i sql/gold_revenue_by_pickup_zone_2024_01.sql
+
+\echo 'Gold layer build complete.'
